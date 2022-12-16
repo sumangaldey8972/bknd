8e5114968774aab943ca3f91da083d3825d93685
sumangaldey8972 <sumangaldey8972@gmail.com>
2022-12-16 12:14:17 +0530

    done

--- user.routes.js
+++ user.routes.js
@@ -20,8 +20,21 @@ app.post("/", async (req, res) => {
       res.status(401).send({ message: "Email already exist" });
     } else {
       let newUser = await userModel.create(req.body);
-      res.status(200).send({ message: "User added successfull", data: newUser });
+      res
+        .status(200)
+        .send({ message: "User added successfull", data: newUser });
     }
+  } catch (err) {
+    res.status(500).send({ massage: "Server error from post user" });
+  }
+});
+
+app.delete("/:id", async (req, res) => {
+  try {
+    let { id } = req.params;
+    let user = await userModel.findOne({ _id: id });
+    await user.deleteOne({ _id: id });
+    res.status(200).send({ message: "user deleted" });
   } catch (err) {
     res.status(500).send({ massage: "Server error from post user" });
   }
